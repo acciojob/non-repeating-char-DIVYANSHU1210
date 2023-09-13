77dd6c6779e637766b40cece6e9b6b5a92dc48ce
DIVYANSHU1210 <84267089+DIVYANSHU1210@users.noreply.github.com>
2023-09-13 12:43:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-}
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+	let arr = [];
+	for(let i = 0; i<str.length() ; i++){
+		arr[str.charAt(i)]++;
+	}
+
+	for(let i=0; i<arr.length(); i++){
+		if(arr[i] == 1)return i.toString();
+	} 
+} 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input));  
